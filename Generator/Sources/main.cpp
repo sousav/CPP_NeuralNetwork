a6940b95d245e151571cc882ad8c730f0a3e341b
Victor Sousa <victor.sousa@epitech.eu>
2018-05-06 23:07:20 +0200

    [-] fix topology

--- Generator/Sources/main.cpp
+++ Generator/Sources/main.cpp
@@ -4,7 +4,7 @@
  * @Date:   14/04/2018 18:09:05
  *
  * @Last modified by:   vicostudio
- * @Last modified time: 22/04/2018 03:31:12
+ * @Last modified time: 06/05/2018 23:07:08
  */
 
 #include <Logger/Manager.h>
@@ -19,7 +19,6 @@ int main(int argc, char *argv[]) {
     Log::Config::addOutput(configList, "OutputConsole");
     Log::Manager::configure(configList);
     Log::Logger logger("Main.cpp");
-    logger.notice() << "Running " << PROJECT_NAME;
 
     MainClass main(argc, argv);
     return main.AMain::Run();
